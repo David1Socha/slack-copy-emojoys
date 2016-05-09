8a00a050f2828e1c12cbfe26df922f508cbf418c
David Socha <david1socha@gmail.com>
2016-05-09 17:04:31 -0500

    Add download.rb

---
+++ download.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+require 'rest-client'
+require 'open-uri'
+#by default ruby doesn't install the ssl cert file, follow https://gist.github.com/fnichol/867550 if needed
+
+team_name = ARGV[0]
+cookie = ARGV[1]
+
+resp = RestClient.get("https://#{team_name}.slack.com/customize/emoji", Cookie: cookie)
+html = Nokogiri::HTML(resp)
+
+emojoy_regex = /https:\/\/.*(\/.*\/).*(png|jpg|gif)/
+emojoy_links = html.css('span.lazy.emoji-wrapper').map {|s| s['data-original'] }
+emojoy_names = emojoy_links.map {|l| emojoy_regex.match(l)[1][1...-1] }
+emojoy_formats = emojoy_links.map {|l| emojoy_regex.match(l)[2] }
+
+emojoys = Array.new
+emojoy_links.each_index do |i|
+  filename = "#{emojoy_names[i]}.#{emojoy_formats[i]}"
+  IO.copy_stream(open(emojoy_links[i]), filename)
+  emojoys << {name: emojoy_names[i], link: emojoy_links[i], local: filename}
+end
+
+File.open("emojoys.txt", "w+") {|file| file.puts emojoys}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Simple ruby scripts for batch downloading and uploading (TODO) of slack emotes.
+---
+
+download.rb
+-format: 'ruby download.rb <team> <cookie>'
+  e.g. 'ruby download.rb raikesdesignstudio "cookie12345"'
+-This script downloads all emotes from the specified team to the current directory.
