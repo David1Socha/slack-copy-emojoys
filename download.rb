f99db1eddc2d542186b93597186d73c050992ab0
David Socha <david1socha@gmail.com>
2016-05-12 20:43:44 -0500

    Download emotes to separate directory

--- download.rb
+++ download.rb
@@ -1,10 +1,13 @@
 require 'nokogiri'
 require 'rest-client'
 require 'open-uri'
-#by default ruby doesn't install the ssl cert file, follow https://gist.github.com/fnichol/867550 if needed
+#by default (at least on windows) ruby doesn't install the ssl cert file, follow https://gist.github.com/fnichol/867550 if needed
 
 team_name = ARGV[0]
 cookie = ARGV[1]
+
+IMG_FOLDER = "img"
+Dir.mkdir IMG_FOLDER if !Dir.exist? IMG_FOLDER
 
 resp = RestClient.get("https://#{team_name}.slack.com/customize/emoji", Cookie: cookie)
 html = Nokogiri::HTML(resp)
@@ -16,7 +19,7 @@ emojoy_formats = emojoy_links.map {|l| emojoy_regex.match(l)[2] }
 
 emojoys = Array.new
 emojoy_links.each_index do |i|
-  filename = "#{emojoy_names[i]}.#{emojoy_formats[i]}"
+  filename = "#{IMG_FOLDER}/#{emojoy_names[i]}.#{emojoy_formats[i]}"
   IO.copy_stream(open(emojoy_links[i]), filename)
   emojoys << {name: emojoy_names[i], link: emojoy_links[i], local: filename}
 end
